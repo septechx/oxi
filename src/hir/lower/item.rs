af4686091bb52d50e5c80f41a364e17ffb603ae2
septech <septechx@proton.me>
2026-06-01 19:30:41 +0200

    refactor: Refactor item lowering

--- src/hir/lower/item.rs
+++ src/hir/lower/item.rs
@@ -1,6 +1,6 @@
 use thin_vec::ThinVec;
 
-use crate::ast::{self, Visibility};
+use crate::ast::{self, Ident, Visibility};
 use crate::hashmap::FxHashMap;
 use crate::hir::owner::{HirId, MaybeOwner, OwnerInfo, OwnerNodes, ParentedNode};
 use crate::hir::types::*;
@@ -9,60 +9,32 @@ use crate::span::Span;
 
 impl<'a, 'ctx> AstLoweringContext<'a, 'ctx> {
     pub(super) fn lower_item(&mut self, def_id: DefId, item: &'a ast::Item, hir_crate: &mut Crate) {
-        let hir_id = HirId::make_owner(def_id);
-        let owner_id = OwnerId(def_id.0);
-
-        match &item.kind {
-            ast::ItemKind::Const { name, ty, value } => {
-                self.current_owner = Some(owner_id);
-                self.next_local_id = 1;
-
-                let ty = self.lower_type(ty);
-                let init = self.lower_expr(value);
-
-                let local_id = self.next_local_id();
-                let body_id = BodyId(local_id);
-                let bodies = {
-                    let mut bodies = FxHashMap::default();
-                    bodies.insert(body_id, Body { value: init });
-                    bodies
-                };
-
-                let hir_item = Item {
-                    hir_id,
-                    owner_id,
-                    kind: ItemKind::Const {
-                        name: name.value,
-                        ty,
-                        body_id: Some(body_id),
-                    },
-                    span: item.span,
-                    visibility: item.visibility,
-                };
-                hir_crate.ensure_owner(def_id);
-                if let Some(owner) = hir_crate.owner_mut(def_id) {
-                    *owner = MaybeOwner::Owner(Box::new(OwnerInfo {
-                        nodes: OwnerNodes {
-                            nodes: vec![ParentedNode {
-                                parent: ItemLocalId::ZERO,
-                                node: Node::Item(Box::new(hir_item)),
-                            }],
-                            bodies,
-                        },
-                        parenting: FxHashMap::default(),
-                    }));
+        self.with_owner(def_id, hir_crate, |this, def_id| {
+            Some(match &item.kind {
+                ast::ItemKind::Const { name, ty, value } => {
+                    this.lower_const(def_id, name, ty, value, item.span, item.visibility)
                 }
+                ast::ItemKind::Fn(f) => this.lower_fn(def_id, f, item.span, item.visibility),
+                ast::ItemKind::Struct { .. } => todo!("Implement lowering of struct items"),
+                ast::ItemKind::Interface { .. } => todo!("Implement lowering of interface items"),
+                ast::ItemKind::Impl { .. } => todo!("Implement lowering of impl items"),
+                ast::ItemKind::Import(_) | ast::ItemKind::Module { .. } => return None,
+            })
+        })
+    }
 
-                self.current_owner = None;
-            }
-            ast::ItemKind::Fn(f) => {
-                self.lower_fn(def_id, f, None, item.span, item.visibility, hir_crate);
-            }
-            ast::ItemKind::Struct { .. } => todo!("Implement lowering of struct items"),
-            ast::ItemKind::Interface { .. } => todo!("Implement lowering of interface items"),
-            ast::ItemKind::Impl { .. } => todo!("Implement lowering of impl items"),
-            ast::ItemKind::Import(_) => {}
-            ast::ItemKind::Module { .. } => {}
+    fn with_owner(
+        &mut self,
+        def_id: DefId,
+        hir_crate: &mut Crate,
+        f: impl FnOnce(&mut Self, DefId) -> Option<OwnerInfo>,
+    ) {
+        self.current_owner = Some(OwnerId(def_id.0));
+        self.next_local_id = 1;
+        if let Some(info) = f(self, def_id) {
+            self.store_owner(def_id, hir_crate, info);
+        } else {
+            self.current_owner = None;
         }
     }
 
@@ -73,9 +45,64 @@ impl<'a, 'ctx> AstLoweringContext<'a, 'ctx> {
         hir_crate: &mut Crate,
     ) {
         match &item.kind {
-            ast::AssocItemKind::Fn(f) => {
-                self.lower_fn(def_id, f, None, item.span, item.visibility, hir_crate);
+            ast::AssocItemKind::Fn(_f) => {
+                _ = def_id;
+                _ = hir_crate;
+                todo!()
             }
+        }
+    }
+
+    fn store_owner(&mut self, def_id: DefId, hir_crate: &mut Crate, owner_info: OwnerInfo) {
+        self.current_owner = None;
+        hir_crate.ensure_owner(def_id);
+        if let Some(owner) = hir_crate.owner_mut(def_id) {
+            *owner = MaybeOwner::Owner(Box::new(owner_info));
+        }
+    }
+
+    fn lower_const(
+        &mut self,
+        def_id: DefId,
+        name: &'a Ident,
+        ty: &'a ast::Type,
+        value: &'a ast::Expr,
+        span: Span,
+        visibility: Visibility,
+    ) -> OwnerInfo {
+        let owner_id = OwnerId(def_id.0);
+        let hir_id = HirId::make_owner(def_id);
+
+        let ty = self.lower_type(ty);
+        let init = self.lower_expr(value);
+
+        let local_id = self.next_local_id();
+        let body_id = BodyId(local_id);
+        let bodies = {
+            let mut bodies = FxHashMap::default();
+            bodies.insert(body_id, Body { value: init });
+            bodies
+        };
+
+        OwnerInfo {
+            nodes: OwnerNodes {
+                nodes: vec![ParentedNode {
+                    parent: ItemLocalId::ZERO,
+                    node: Node::Item(Box::new(Item {
+                        hir_id,
+                        owner_id,
+                        kind: ItemKind::Const {
+                            name: name.value,
+                            ty,
+                            body_id: Some(body_id),
+                        },
+                        span,
+                        visibility,
+                    })),
+                }],
+                bodies,
+            },
+            parenting: FxHashMap::default(),
         }
     }
 
@@ -83,16 +110,11 @@ impl<'a, 'ctx> AstLoweringContext<'a, 'ctx> {
         &mut self,
         def_id: DefId,
         f: &'a ast::Fn,
-        _associated: Option<DefId>,
         span: Span,
         visibility: Visibility,
-        hir_crate: &mut Crate,
-    ) {
-        let hir_id = HirId::make_owner(def_id);
+    ) -> OwnerInfo {
         let owner_id = OwnerId(def_id.0);
-
-        self.current_owner = Some(owner_id);
-        self.next_local_id = 1;
+        let hir_id = HirId::make_owner(def_id);
 
         let params: ThinVec<Param> = f
             .parameters
@@ -118,31 +140,28 @@ impl<'a, 'ctx> AstLoweringContext<'a, 'ctx> {
             body_id
         });
 
-        let sig = FnSig {
-            is_extern: f.is_extern,
-        };
-        let decl = FnDecl { params, ret };
-
-        let nodes = vec![ParentedNode {
-            parent: ItemLocalId::ZERO,
-            node: Node::Item(Box::new(Item {
-                hir_id,
-                owner_id,
-                kind: ItemKind::Fn { sig, decl, body_id },
-                span,
-                visibility,
-            })),
-        }];
-
-        hir_crate.ensure_owner(def_id);
-        if let Some(owner) = hir_crate.owner_mut(def_id) {
-            *owner = MaybeOwner::Owner(Box::new(OwnerInfo {
-                nodes: OwnerNodes { nodes, bodies },
-                parenting: FxHashMap::default(),
-            }));
+        OwnerInfo {
+            nodes: OwnerNodes {
+                nodes: vec![ParentedNode {
+                    parent: ItemLocalId::ZERO,
+                    node: Node::Item(Box::new(Item {
+                        hir_id,
+                        owner_id,
+                        kind: ItemKind::Fn {
+                            sig: FnSig {
+                                is_extern: f.is_extern,
+                            },
+                            decl: FnDecl { params, ret },
+                            body_id,
+                        },
+                        span,
+                        visibility,
+                    })),
+                }],
+                bodies,
+            },
+            parenting: FxHashMap::default(),
         }
-
-        self.current_owner = None;
     }
 
     fn lower_block_body(&mut self, block: &ast::Block) -> (BodyId, Body) {
